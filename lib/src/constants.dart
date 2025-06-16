28f38cc3f23470166094e529d94de9ec88227665
Amrazyan1 <arman.amrazyan@softconstruct.com>
2025-06-16 12:46:33 +0400

    Update constants.dart

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -28,10 +28,10 @@ class Timeouts {
   });
 
   static const Timeouts defaultTimeouts = Timeouts(
-    connection: Duration(seconds: 10),
+    connection: Duration(seconds: 30),
     debounce: Duration(milliseconds: 100),
-    publish: Duration(seconds: 10),
-    peerConnection: Duration(seconds: 10),
-    iceRestart: Duration(seconds: 10),
+    publish: Duration(seconds: 30),
+    peerConnection: Duration(seconds: 30),
+    iceRestart: Duration(seconds: 30),
   );
 }
